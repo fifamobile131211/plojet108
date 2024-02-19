22eaf2fc4ca5d4dabb371dd76ec490ba2342df52
fifamobile131211 <143459030+fifamobile131211@users.noreply.github.com>
2024-02-19 09:43:41 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+setTimeout(function()
+   {
+   img_id="selfie1";
+   take_snapshot();
+   speak_data ="tirando sua selfie em 10 segundos";
+   var utterthis = new SpeechSynthesisUtterance(speak_data);
+   synth.speak(utterthis);
+    
+  },  5000 );
+
+
+  function take_snapshot()
+{
+   console.log(img_id);
+
+
+Webcam.snap(function(data_uri){
+       if(img_id =="selfie1"){
+        document.getElementById("result1").innerHTML = '<img id ="selfie1" src="'+data_uri+'"/>';
+       }
+       if(img_id =="selfie2"){
+        document.getElementById("result2").innerHTML = '<img id ="selfie2" src="'+data_uri+'"/>';
+       }
+       if(img_id =="selfie3"){
+        document.getElementById("result3").innerHTML = '<img id ="selfie3" src="'+data_uri+'"/>';
+       }
+
+
+
+                   })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
